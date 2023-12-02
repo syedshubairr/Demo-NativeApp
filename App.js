961221973ae490b220bdb2f22aefdac9ff904a2c
syedshubairr <syedshubair8299@gmail.com>
2023-12-02 18:41:34 +0500

    Added Text Input and Button

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View>
-      <View>
-        <Text>Nested Test</Text>
+      <View
+        style={{
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: 30,
+        }}
+      >
+        <TextInput placeholder="Enter name" />
+        <Button title="Click" />
       </View>
-      <Text>Hey there</Text>
+      <Text
+        style={{
+          display: "flex",
+          textAlign: "center",
+          alignContent: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+          fontSize: 30,
+        }}
+      >
+        Welcome
+      </Text>
     </View>
   );
 }
