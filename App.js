5e9ced0549698e1ebb7f84914e1089f555aa9678
syedshubairr <syedshubair8299@gmail.com>
2023-12-03 20:34:38 +0500

    Added State Management with List Data Displaying.

--- App.js
+++ App.js
@@ -1,32 +1,61 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleText = (text) => {
+    setName(text);
+  };
+
+  const handleButton = () => {
+    // console.log(name);
+    setList((currentList) => [...currentList, name]);
+  };
+
+  const handleClear = () => {
+    setList([]);
+  };
+  console.log(list);
+
   return (
-    <View>
-      <View
-        style={{
-          display: "flex",
-          alignContent: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-          padding: 30,
-        }}
-      >
-        <TextInput placeholder="Enter name" />
-        <Button title="Click" />
+    <View style={style.main}>
+      <View style={style.container}>
+        <TextInput
+          onChangeText={handleText}
+          placeholder="Enter name"
+          style={style.input}
+        />
+        <Button onPress={handleButton} title="Add Note" color="#000" />
       </View>
-      <Text
-        style={{
-          display: "flex",
-          textAlign: "center",
-          alignContent: "center",
-          flexDirection: "column",
-          justifyContent: "center",
-          fontSize: 30,
-        }}
-      >
-        Welcome
-      </Text>
+      <View style={style.listContainer}>
+        <Button onPress={handleClear} title="Clear List" color="#AAA" />
+      </View>
+      <View style={style.listContainer}>
+        {list.map((item, index) => (
+          <Text key={index}>{item}</Text>
+        ))}
+      </View>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  main: {
+    padding: 60,
+  },
+  container: {
+    flexDirection: "row",
+    paddingBottom: 30,
+    borderBottomWidth: 1,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    flex: 1,
+  },
+  listContainer: {
+    padding: 20,
+  },
+});
